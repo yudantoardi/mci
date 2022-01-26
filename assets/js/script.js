dfc8620642e8f8976342a25bd6626d631d9a8fee
Winda Pra <57739210+hiwinda@users.noreply.github.com>
2022-01-26 20:09:23 +0700

    styling homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,18 +36,21 @@ $(document).ready(function(){
             loop:true,
 			margin:0,
             dots: false,
+			nav: false,
 			smartSpeed: 1000,
 			autoplay: 500,
-			navText: [ '<span class="fas fa-arrow-left"></span>', '<span class="fas fa-arrow-right"></span>' ],
 			responsive:{
 				0:{
 					items:1
 				},
-				768:{
+				576:{
 					items:2
 				},
-				992:{
+				768:{
 					items:3
+				},
+				992:{
+					items:4
 				}
 			}
         })		
